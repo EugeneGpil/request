a80aaccdbb881e2e5616a271615e73ddd651c93f
EugeneGpil <gpilligrim@gmail.com>
2023-10-17 05:17:23 +0000

    1

---
+++ app/modules/DecodeBody/tests/should_return_correct_body/should_return_correct_body_test.go
@@ -0,0 +1,38 @@
+package should_return_correct_body
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/EugeneGpil/tester"
+
+	httpTester "github.com/EugeneGpil/httpTester"
+	requestPackage "github.com/EugeneGpil/request"
+)
+
+type body struct {
+	Test string
+}
+
+var testValue = "test"
+
+func Test_should_return_correct_body(t *testing.T) {
+	tester.SetTester(t)
+
+	httpRequest := httpTester.GetRequest(httpTester.GetRequestDto{
+		Method: http.MethodGet,
+		Url:    "/test",
+		Body: body{
+			Test: testValue,
+		},
+	})
+
+	request := requestPackage.New(&httpRequest)
+
+	var bodyVar body
+
+	err := request.DecodeBody(&bodyVar)
+
+	tester.AssertNil(err)
+	tester.AssertSame(bodyVar.Test, testValue)
+}
---
+++ request.go
@@ -0,0 +1,22 @@
+package request
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Request struct {
+	request *http.Request
+}
+
+func New(request *http.Request) Request {
+	return Request{
+		request: request,
+	}
+}
+
+func (request *Request) DecodeBody(body interface{}) error {
+	err := json.NewDecoder(request.request.Body).Decode(&body)
+
+	return err
+}
